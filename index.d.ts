2b3f1cf105452f6de143ff4defa4bd224a8b7981
botbye <accounts@botbye.com>
2025-10-27 14:27:36 +0300

    update

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export { BotByeVuePlugin } from './plugin';
 export { BotByeComponent } from './component';
+export { runChallenge, type TChallengesRunner, type TGetTokenOptions } from "botbye-client";
